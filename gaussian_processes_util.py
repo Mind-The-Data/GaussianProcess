1cd9db09786bd8d16fa649864eeb13a10d8ee05d
Mind-The-Data <brian.stampe@umontana.edu>
2019-10-02 23:22:10 -0600

    Gp from Numpy on WW --its a start

--- gaussian_processes_util.py
+++ gaussian_processes_util.py
@@ -25,3 +25,45 @@ def plot_gp_2D(gx, gy, mu, X_train, Y_train, title, i):
     ax.plot_surface(gx, gy, mu.reshape(gx.shape), cmap=cm.coolwarm, linewidth=0, alpha=0.2, antialiased=False)
     ax.scatter(X_train[:,0], X_train[:,1], Y_train, c=Y_train, cmap=cm.coolwarm)
     ax.set_title(title)
+    
+def nll_fn(X_train, Y_train, noise, naive=True):
+    '''
+    Returns a function that computes the negative marginal log-
+    likelihood for training data X_train and Y_train and given 
+    noise level.
+    
+    Args:
+        X_train: training locations (m x d).
+        Y_train: training targets (m x 1).
+        noise: known noise level of Y_train.
+        naive: if True use a naive implementation of Eq. (7), if 
+               False use a numerically more stable implementation. 
+        
+    Returns:
+        Minimization objective.
+    '''
+    def nll_naive(theta):
+        # Naive implementation of Eq. (7). Works well for the examples 
+        # in this article but is numerically less stable compared to 
+        # the implementation in nll_stable below.
+        K = kernel(X_train, X_train, l=theta[0], sigma_f=theta[1]) + \
+            noise**2 * np.eye(len(X_train))
+        return 0.5 * np.log(det(K)) + \
+               0.5 * Y_train.T.dot(inv(K).dot(Y_train)) + \
+               0.5 * len(X_train) * np.log(2*np.pi)
+
+    def nll_stable(theta):
+        # Numerically more stable implementation of Eq. (7) as described
+        # in http://www.gaussianprocess.org/gpml/chapters/RW2.pdf, Section
+        # 2.2, Algorithm 2.1.
+        K = kernel(X_train, X_train, l=theta[0], sigma_f=theta[1]) + \
+            noise**2 * np.eye(len(X_train))
+        L = cholesky(K)
+        return np.sum(np.log(np.diagonal(L))) + \
+               0.5 * Y_train.T.dot(lstsq(L.T, lstsq(L, Y_train)[0])[0]) + \
+               0.5 * len(X_train) * np.log(2*np.pi)
+    
+    if naive:
+        return nll_naive
+    else:
+        return nll_stable
